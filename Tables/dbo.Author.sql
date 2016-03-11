ee5a56414a50fae2556a9a2e492ec7f183b99ff7
Sjors Takes <sjors.takes@albumprinter.com>
2016-03-11 10:25:02 +0100

    CREATE TABLE Author

---
+++ Tables/dbo.Author.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Author]
+(
+[id] [int] NULL,
+[firstname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[lastname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
