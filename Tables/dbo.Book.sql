0d10013bf2ca02ba46537f2a653948a9722cc7eb
Sjors Takes <sjors.takes@albumprinter.com>
2016-03-11 10:57:20 +0100

    ALTER TABLE dbo.Book ADD CONSTRAINT FK_Book_Author

--- Tables/dbo.Book.sql
+++ Tables/dbo.Book.sql
@@ -4,4 +4,6 @@ CREATE TABLE [dbo].[Book]
 [title] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [AuthorID] [int] NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Book] ADD
+CONSTRAINT [FK_Book_Author] FOREIGN KEY ([AuthorID]) REFERENCES [dbo].[Author] ([id]) ON DELETE CASCADE ON UPDATE CASCADE
 GO
