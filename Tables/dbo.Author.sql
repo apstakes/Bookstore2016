cd466e3c6e23e062579e89d17c855713e5b760a0
Sjors Takes <sjors.takes@albumprinter.com>
2016-03-11 10:55:18 +0100

    ALTER TABLE dbo.Author ADD CONSTRAINT PK_Author_id PRIMARY KEY (id)

--- Tables/dbo.Author.sql
+++ Tables/dbo.Author.sql
@@ -4,4 +4,6 @@ CREATE TABLE [dbo].[Author]
 [firstname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [lastname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Author] ADD 
+CONSTRAINT [PK_Author_id] PRIMARY KEY CLUSTERED  ([id]) ON [PRIMARY]
 GO
